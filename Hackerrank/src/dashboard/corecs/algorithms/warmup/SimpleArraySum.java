299dc7a9e54ff7de3ecaec45b2e99c0e23c47aa3
Sambit Bhadra <undisputedleader@gmail.com>
2017-08-19 20:24:45 +0530

    Hacker Rank stuff

---
+++ Hackerrank/src/dashboard/corecs/algorithms/warmup/SimpleArraySum.java
@@ -0,0 +1,30 @@
+package dashboard.corecs.algorithms.warmup;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SimpleArraySum {
+
+    static int simpleArraySum(int n, int[] ar) {
+        // Complete this function
+        int sum = 0;
+        for(int a : ar){
+            sum = sum+a;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = simpleArraySum(n, ar);
+        System.out.println(result);
+    }
+}
